0ac8f4acd065d800228450e475d0541efc78e15e
Steve-Mcl <sdmclaughlin@gmail.com>
2025-06-26 11:24:36 +0100

    Add new completions plugin

---
+++ completions.js
@@ -0,0 +1,6 @@
+module.exports = (RED) => {
+    // Dummy module.
+    // This causes NR to load the associated html part of the plugin where the client side
+    // code for completions are defined.
+    // The actual completions logic is in the lib/assistant.js file.
+}
