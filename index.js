71b5e083c33fdc53a08e268c9548c0e6df3597e0
Nick O'Leary <nick.oleary@gmail.com>
2025-12-09 14:13:26 +0000

    Rebrand to Expert

--- index.js
+++ index.js
@@ -4,22 +4,27 @@ module.exports = (RED) => {
     const auth = require('./lib/auth/index.js')
     RED.plugins.registerPlugin('flowfuse-nr-assistant', {
         type: 'assistant',
-        name: 'Node-RED Assistant Plugin',
+        name: 'Node-RED Expert Plugin',
         icon: 'font-awesome/fa-magic',
         settings: {
             '*': { exportable: true }
         },
         onadd: async function () {
-            await auth.init(RED)
-            const assistantSettings = await settings.getSettings(RED)
-            if (!assistant.isInitialized && !assistant.isLoading) {
-                assistant.init(RED, assistantSettings).then(() => {
-                    // All good, the assistant is initialized.
-                    // Any info messages made during initialization are logged in the assistant module
-                }).catch((error) => {
-                    console.error(error)
-                    RED.log.error('Failed to initialize FlowFuse Assistant Plugin:', error)
-                })
+            try {
+                await auth.init(RED)
+                const assistantSettings = await settings.getSettings(RED)
+                if (!assistant.isInitialized && !assistant.isLoading) {
+                    assistant.init(RED, assistantSettings).then(() => {
+                        // All good, the assistant is initialized.
+                        // Any info messages made during initialization are logged in the assistant module
+                    }).catch((error) => {
+                        console.error(error)
+                        RED.log.error('Failed to initialize FlowFuse Expert Plugin:', error)
+                    })
+                }
+            } catch (error) {
+                console.error(error)
+                RED.log.error('Failed to initialize FlowFuse Expert Plugin:', error)
             }
         }
     })
